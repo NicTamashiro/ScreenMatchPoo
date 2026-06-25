6ee6717ff48d65e703c70e31e50ce9d21946f284
Nicolas <adilsonq1976@gmail.com>
2026-06-25 14:50:42 -0300

    Criando construtor para a classe Filme

--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -5,6 +5,11 @@ import br.com.alura.screenmatch.calculos.Classificavel;
 public class Filme extends Titulo implements Classificavel {
     private String diretor;
 
+    public Filme(String diretor, int anoDeLancamento) {
+        super(diretor, anoDeLancamento);
+        this.setNome(diretor);
+    }
+
     public String getDiretor() {
         return diretor;
     }
