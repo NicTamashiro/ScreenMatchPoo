6dbd4493167b8673e718174ff6eacd62a0357640
Nicolas <adilsonq1976@gmail.com>
2026-06-25 14:51:19 -0300

    Utilizando os construtores criados na classe Main

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Filme meuFilme = new Filme();
-        meuFilme.setNome("O poderoso chefao");
-        meuFilme.setAnoDeLancamento(1970);
+        Filme meuFilme = new Filme("O poderoso chefao", 1970);
         meuFilme.setDuracaoEmMinutos(180);
         System.out.println("Duracao do filme: " + meuFilme.getDuracaoEmMinutos());
 
@@ -20,18 +18,14 @@ public class Main {
         meuFilme.avalia(10);
         System.out.println("Media de avaliacoes do filme: " + meuFilme.pegaMedia());
 
-        Serie minhaSerie = new Serie();
-        minhaSerie.setNome("Lost");
-        minhaSerie.setAnoDeLancamento(2000);
+        Serie minhaSerie = new Serie("Lost", 2000);
         minhaSerie.exibeFichaTecnica();
         minhaSerie.setTemporadas(10);
         minhaSerie.setEpisodiosPorTemporada(10);
         minhaSerie.setMinutosPorEpisodio(50);
         System.out.println("Duracao para maratonar Lost: " + minhaSerie.getDuracaoEmMinutos());
 
-        Filme outroFilme = new Filme();
-        outroFilme.setNome("Avatar");
-        outroFilme.setAnoDeLancamento(2023);
+        Filme outroFilme = new Filme("Avatar", 2023);
         outroFilme.setDuracaoEmMinutos(200);
 
         CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
@@ -49,9 +43,7 @@ public class Main {
         episodio.setTotalVisualizacoes(300);
         filtro.filtra(episodio);
 
-        Filme filmeDoPaulo = new Filme();
-        filmeDoPaulo.setNome("Dogville");
-        filmeDoPaulo.setAnoDeLancamento(2003);
+        Filme filmeDoPaulo = new Filme("Dogville", 2003);
         filmeDoPaulo.setDuracaoEmMinutos(200);
         filmeDoPaulo.avalia(10);
 
