e3415467c8c5e61648308acd2a58d76f5a7070d7
Nicolas <adilsonq1976@gmail.com>
2026-06-30 11:53:07 -0300

    Implementando uma scanner e concatenando o endereco

--- src/br/com/alura/screenmatch/Principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/Principal/PrincipalComBusca.java
@@ -5,11 +5,19 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um filme para busca: ");
+        String busca = sc.nextLine();
+
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=2f65f5b5";
+
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://www.omdbapi.com/?t=Matrix&apikey=2f65f5b5")).build();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
 
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
